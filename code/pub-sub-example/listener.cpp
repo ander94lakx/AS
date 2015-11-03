69b247f3970f94bd4cbc0a8d1f637407765ba2ad
ander94lakx <andergranado94@gmail.com>
2015-11-03 20:46:56 +0100

    Quita espacios entre lineas

--- code/pub-sub-example/listener.cpp
+++ code/pub-sub-example/listener.cpp
@@ -9,12 +9,8 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener");
-
   ros::NodeHandle n;
-
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-
   ros::spin();
-
   return 0;
 }
